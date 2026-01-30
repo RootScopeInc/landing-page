e37aa18e2499eb6c56a35db897b156f2589a4be9
Hrithvik <gadhiyahrithvik@gmail.com>
2026-01-30 09:49:05 +0100

    update support email

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
         </div>
         <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
           <p>© 2025 RootScope Inc. All rights reserved.</p>
-          <Link href="mailto:support@rootscope.io">support@rootscope.ca</Link>
+          <Link href="mailto:support@rootscope.io">support@rootscope.io</Link>
           <ul className="flex flex-wrap items-center gap-4 mt-6 sm:text-sm sm:mt-0">
             {footerLinks.map((item) => (
               <li key={item.label}>
